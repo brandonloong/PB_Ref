9e409505f38170cc035da8f305634de264e80173
Brandon Loong <brandon.loong@gmail.com>
2019-02-05 06:58:20 -0800

    Update UI and partially enable MatchSumVC display functionality with current match (only 1st game works right now). Still need to programmatically add game displays to view

--- PB_Ref/Controllers/MatchSummaryViewController.swift
+++ PB_Ref/Controllers/MatchSummaryViewController.swift
@@ -17,7 +17,7 @@ class MatchSummaryViewController: UIViewController {
 	@IBOutlet weak var p2Label: UILabel!
 	@IBOutlet weak var p3Label: UILabel!
 	@IBOutlet weak var p4Label: UILabel!
-	@IBOutlet weak var g1TimeLabel: UILabel!
+	@IBOutlet weak var g1ClockLabel: UILabel!
 	@IBOutlet weak var g1RallyLabel: UILabel!
 	@IBOutlet weak var g1T1Runner: UILabel!
 	@IBOutlet weak var g1T2Runner: UILabel!
@@ -60,13 +60,22 @@ class MatchSummaryViewController: UIViewController {
 		winLabels = [t1WinLabel,t2WinLabel]
 		pLabels = [p1Label,p2Label,p3Label,p4Label]
 		
-		// Setup view
+		// Match summary
 		if let winner = match.matchWinner {
-			winLabels[winner-1].text = "Won"
-			winLabels[(winner)%2].text = "Lost"
+			winLabels[winner].text = "Won"
+			winLabels[(winner+1)%2].text = "Lost"
 		}
 		for i in 0...3 {pLabels[i].text = match.playerNames[i]}	// update pLabels
 		
+		// Game statistics
+		g1ClockLabel.text = "00:00"
+		g1RallyLabel.text = "Rallies: \(match.rCount)"
+		g1T1Runner.text = match.runnerText[0]
+		g1T2Runner.text = match.runnerText[1]
+		g1T1TOLabel.text = "\(match.timeOuts[0])"
+		g1T2TOLabel.text = "\(match.timeOuts[1])"
+		
+		// Team statistics
 		
     }
     override func didReceiveMemoryWarning() {
